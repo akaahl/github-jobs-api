5c81791d87f9daf1b80b31bacb316191e843efe4
a.k.a.a.h.l <a.k.afiq@live.com>
2021-04-07 21:58:42 +0800

    Another commit

--- script.js
+++ script.js
@@ -2,11 +2,24 @@
 // const darkModeBtn = document.querySelector(".dark-mode");
 const modeBtns = document.querySelectorAll(".mode-btn");
 const lightAndDarkModeCheckbox = document.querySelector(".light-dark-checkbox");
+const mainContainer = document.getElementById("main-container");
 
-modeBtns.forEach((button) => {
-  if (button.classList.contains("light")) {
-  }
+// modeBtns.forEach((button) => {
+//   if (button.classList.contains("light")) {
+//   }
 
-  if (button.classList.contains("dark")) {
-  }
-});
+//   if (button.classList.contains("dark")) {
+//   }
+// });
+
+const proxy = `https://cors-anywhere.herokuapp.com/`;
+const apiUrl = `https://jobs.github.com/positions.json?`;
+
+async function getJobsData(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  console.log(data);
+}
+
+getJobsData(apiUrl);
